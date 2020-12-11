47aef3b77f1c964f788da8b9e4621fa11c0317be
bruteforce1414 <bruteforce1414@gmail.com>
2020-12-11 13:39:11 +0500

    feat: fix imports in gorm

--- public/sql/gorm/gorm.go
+++ public/sql/gorm/gorm.go
@@ -2,17 +2,17 @@ package pg
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/nori-io/common/v3/pkg/domain/meta"
+	"github.com/nori-io/common/v3/pkg/errors"
+	"github.com/nori-io/common/v3/pkg/domain/registry"
 
-	"github.com/nori-io/common/v3/errors"
-	"github.com/nori-io/common/v3/plugin"
-	"github.com/nori-io/common/v3/meta"
 )
 
 const (
 	GormInterface meta.Interface = "public/sql/Gorm@v1.9.15"
 )
 
-func GetGorm(r plugin.Registry) (*gorm.DB, error) {
+func GetGorm(r registry.Registry) (*gorm.DB, error) {
 	instance, err := r.Interface(GormInterface)
 	if err != nil {
 		return nil, err
