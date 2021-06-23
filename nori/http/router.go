349f68b6651db5a57dacf37382600fcd829738cd
Serge <serge@cheebo.me>
2021-06-23 08:23:10 +0500

    refactor: renamed http router file, code prettifications

--- nori/http/router.go
+++ nori/http/router.go
@@ -8,16 +8,20 @@ import (
 	"github.com/nori-io/common/v5/pkg/errors"
 )
 
-const RouterInterface meta.Interface = "nori/http/Router@2.0.0"
+const RouterInterface meta.Interface = "nori/http/Router@1.0.0"
 
 type Router interface {
 	http.Handler
+
 	Handle(pattern string, h http.Handler)
 	HandleFunc(pattern string, h http.HandlerFunc)
+
 	Method(method, pattern string, h http.Handler)
 	MethodFunc(method, pattern string, h http.HandlerFunc)
+
 	Route(pattern string, fn func(h Router)) Router
 	Mount(pattern string, h http.Handler)
+
 	Connect(pattern string, h http.HandlerFunc)
 	Delete(pattern string, h http.HandlerFunc)
 	Get(pattern string, h http.HandlerFunc)
@@ -27,10 +31,13 @@ type Router interface {
 	Post(pattern string, h http.HandlerFunc)
 	Put(pattern string, h http.HandlerFunc)
 	Trace(pattern string, h http.HandlerFunc)
+
 	Use(middlewares ...func(http.Handler) http.Handler)
 	With(middlewares ...func(http.Handler) http.Handler) Router
+
 	NotFound(h http.HandlerFunc)
 	MethodNotAllowed(h http.HandlerFunc)
+
 	URLParam(r *http.Request, key string) string
 }
 
